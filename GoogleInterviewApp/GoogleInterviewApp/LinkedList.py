4d1134a9d069cf93f0ef09bc668d59034a3ff4c7
Stanislav Orlov <stanislav.orlov@bd.com>
2024-01-06 19:27:25 +0200

    Linked list

---
+++ GoogleInterviewApp/GoogleInterviewApp/LinkedList.py
@@ -0,0 +1,137 @@
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp != None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.tail = self.tail
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        
+        if index == 0:
+            return self.prepend(value)
+        
+        if index == self.length:
+                return self.append(value)
+        
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.head = self.head.next
+        else:
+            if index == self.length - 1:
+                if self.length < 2:
+                    self.head = None
+                    self.tail = None
+                else:
+                    tmp = self.get(self.length - 2)
+                    self.tail = tmp
+                    tmp.next = None
+            else:
+                tmp = self.get(index)
+                pre = self.get(index - 1)
+                pre.next = tmp.next
+                
+        self.length -= 1
+
+    def reverse(self):
+        before = None
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+
+        while temp is not None:
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+my_linked_list = LinkedList(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.print_list()
+my_linked_list.reverse()
+my_linked_list.print_list()
