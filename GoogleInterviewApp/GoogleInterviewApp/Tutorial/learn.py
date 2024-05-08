25842b814e16d3722f55b1e421adcab797bac511
Stanislav Orlov <stanislav.orlov@bd.com>
2024-05-08 17:54:54 +0300

    queue stack dequeue

--- GoogleInterviewApp/GoogleInterviewApp/Tutorial/learn.py
+++ GoogleInterviewApp/GoogleInterviewApp/Tutorial/learn.py
@@ -42,3 +42,46 @@ print(sum(s in stones for s in jewelsSet))
 
 carry, out = divmod(17,10)
 print(carry, out)
+
+# stack
+stack = []
+stack.append(1)
+stack.append(2)
+print(stack.pop())
+print(stack[len(stack) - 1])
+
+stack2 = []
+stack2.append(10)
+stack2.append(20)
+stack2.append(30)
+print(stack2.pop())
+print(stack2.pop())
+print(stack2.pop())
+
+# queue
+q = []
+# enqueue
+q.append(1)
+q.append(2)
+q.append(3)
+# dequeue
+print(q.pop(0))
+print(q.pop(0))
+print(q.pop(0))
+
+from collections import deque
+
+print("deque - stack")
+data = deque()
+data.append(1)
+data.append(2)
+data.pop()
+print(data)
+
+print("deque - queue")
+data2 = deque()
+data2.append(1)
+data2.append(2)
+data2.append(3)
+data2.popleft()
+print(data2)
