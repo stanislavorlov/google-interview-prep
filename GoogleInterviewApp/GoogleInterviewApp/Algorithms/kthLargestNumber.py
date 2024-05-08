b36284038386a445ff94a03839d12bbb47ff27d5
Stanislav Orlov <stanislav.orlov@bd.com>
2024-05-08 12:20:47 +0300

    Kth largest number in array

--- GoogleInterviewApp/GoogleInterviewApp/Algorithms/kthLargestNumber.py
+++ GoogleInterviewApp/GoogleInterviewApp/Algorithms/kthLargestNumber.py
@@ -1,10 +1,29 @@
+# https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+# Given an integer array nums and an integer k, return the kth largest element in the array.
+# Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+import sys
 from typing import List
 
+def kthLargestNumber(nums: List[int], k: int) -> int:
+    min_v, max_v = sys.maxsize, -sys.maxsize
 
-def kthLargestNumber(nums: List[str], k: int) -> str:
-    
+    for n in nums:
+        min_v = min(min_v, n)
+        max_v = max(max_v, n)
 
-    return ""
+    map = {}
+    for i in nums:
+        map[i] = map.get(i, 0) + 1
 
-nums = ["3","6","7","10"]
+    for i in range(max_v, min_v - 1, -1):
+        if i in map:
+            k -= map[i]
+            if k <= 0:
+                return i
+
+    return -1
+
+nums = [3, 6, 7, 10]
 print(kthLargestNumber(nums, 4))
