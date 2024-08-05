4b32062d6265f799005d595ebd9d102fc83e71ad
Stanislav Orlov <stanislav.orlov@bd.com>
2024-08-05 19:37:51 +0300

    Merge sort

---
+++ GoogleInterviewApp/GoogleInterviewApp/Tutorial/Book/Algorithms_Cormen/mergeSort_iteractive.py
@@ -0,0 +1,48 @@
+def mergeSort(arr):
+    width = 1
+    while width < len(arr) - 1:
+        left = 0
+        while left < len(arr) - 1:
+            mid = left + width - 1
+            right = ((2 * width + left - 1, len(arr) - 1)[2 * width + left - 1 > len(arr)-1])
+            merge(arr, left, mid, right)
+            left = left + width * 2
+        width = 2 * width
+
+def merge(arr, low, mid, high):
+    n1 = mid - low + 1
+    n2 = high - mid
+    left = [0] * n1
+    right = [0] * n2
+    
+    for i in range(0, n1):
+        left[i] = arr[low + i]
+        
+    for i in range(0, n2):
+        right[i] = arr[mid + i + 1] 
+    
+    i, j, k = 0, 0, low
+        
+    while i < n1 and j < n2:
+        if left[i] > right[j]:
+            arr[k] = right[j]
+            j += 1
+        else:
+            arr[k] = left[i]
+            i += 1
+        k += 1
+     
+    while i < n1:
+        arr[k] = left[i]
+        i += 1
+        k += 1
+        
+    while j < n2:
+        arr[k] = right[j]
+        j += 1
+        k += 1
+        
+arr = [12, 11, 13, 5, 6, 7]
+mergeSort(arr)
+
+print(arr)
---
+++ GoogleInterviewApp/GoogleInterviewApp/Tutorial/Book/Algorithms_Cormen/mergeSort_recursive2.py
@@ -0,0 +1,35 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+        
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+        
+        # merge
+        i = j = k = 0
+        
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+            
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+            
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+arr_test = [2,3,5,1,7,4,4,4,2,6,0]
+merge_sort(arr_test)
+
+print(arr_test)
