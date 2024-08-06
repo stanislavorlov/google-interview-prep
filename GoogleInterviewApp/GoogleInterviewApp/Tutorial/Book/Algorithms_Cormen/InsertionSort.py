e26fc6bef1fe02d68bf0dd8585c37b747a213754
Stanislav Orlov <stanislav.orlov@bd.com>
2024-08-06 18:43:14 +0300

    Insertion sort

---
+++ GoogleInterviewApp/GoogleInterviewApp/Tutorial/Book/Algorithms_Cormen/InsertionSort.py
@@ -0,0 +1,12 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1]=key
+
+arr = [6,5,4,3,2,1]
+insertion_sort(arr)
+print(arr)
