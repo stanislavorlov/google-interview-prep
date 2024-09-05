504047cca1d02e6ece78c30c349bc9430f61ad0d
Stanislav Orlov <stanislav.orlov@bd.com>
2024-09-05 13:19:27 +0300

    code clean up

--- GoogleInterviewApp/GoogleInterviewApp/DataStructures/LinkedList/leetcode_tasks/swapPairs.py
+++ GoogleInterviewApp/GoogleInterviewApp/DataStructures/LinkedList/leetcode_tasks/swapPairs.py
@@ -16,22 +16,21 @@ class ListNode:
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(-1)
+        dummy = ListNode(0)
         dummy.next = head
+        first = head
         prev = dummy
 
-        while head and head.next:
-            first = head
-            second = head.next
-
-            prev.next = second
+        while first and first.next:
+            second = first.next
             first.next = second.next
             second.next = first
+            prev.next = second
 
             prev = first
-            head = first.next
+            first = first.next
 
-        return dummy
+        return dummy.next
 
     def print_list(self, head: Optional[ListNode]):
         pointer = head
@@ -41,47 +40,5 @@ class Solution:
 
 solution = Solution()
 list = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
-#solution.swapPairs(list)
-#solution.print_list(list)
-
-
-
-
-def swapPairs(head: Optional[ListNode]) -> Optional[ListNode]:
-    dummy = ListNode(0)
-    dummy.next = head
-    first = head
-    prev = dummy
-
-    while first and first.next:
-        second = first.next
-        first.next = second.next
-        second.next = first
-        prev.next = second
-
-        prev = first
-        first = first.next
-
-    return dummy.next
-
-def swapPairs2(head: Optional[ListNode]) -> Optional[ListNode]:
-    dummyHead = ListNode(0)
-    dummyHead.next = head
-
-    prev = dummyHead
-    first = head
-
-    while first and first.next:
-        second = first.next
-        prev.next = second
-
-        first.next = second.next
-        second.next = first
-
-        prev = first
-        first = first.next
-
-    return dummyHead.next
-
-list = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
-swapPairs2(list)
+list = solution.swapPairs(list)
+solution.print_list(list)
