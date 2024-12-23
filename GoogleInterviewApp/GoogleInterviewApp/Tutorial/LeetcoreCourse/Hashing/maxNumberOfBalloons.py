09d35e0f47ed11aa0648cb7f7a0654de8a2cdb18
Stanislav Orlov <stanislav.orlov@bd.com>
2024-12-23 18:38:02 +0200

    Maximum number of ballons

---
+++ GoogleInterviewApp/GoogleInterviewApp/Tutorial/LeetcoreCourse/Hashing/maxNumberOfBalloons.py
@@ -0,0 +1,25 @@
+# Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+# Input: text = "nlaebolko"
+# Output: 1
+
+# Input: text = "loonbalxballpoon"
+# Output: 2
+
+# Input: text = "leetcode"
+# Output: 0
+
+from typing import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        counter = Counter(text)
+        
+        return min(counter['b'], counter['a'], counter['l'] // 2, counter['o'] // 2, counter['n'])
+    
+solution = Solution()
+print(solution.maxNumberOfBalloons('nlaebolko'))
+
+print(solution.maxNumberOfBalloons('loonbalxballpoon'))
+
+print(solution.maxNumberOfBalloons('leetcode'))
