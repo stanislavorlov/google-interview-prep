7b515a9e1eba0c41366eaacbb489c024f4ff53ba
Stanislav Orlov <stanislav.orlov@bd.com>
2025-01-06 16:18:07 +0200

    Ransom Note

---
+++ GoogleInterviewApp/GoogleInterviewApp/Tutorial/LeetcoreCourse/Hashing/canConstruct.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/explore/featured/card/leetcodes-interview-crash-course-data-structures-and-algorithms/705/hashing/4607/
+
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed 
+# by using the letters from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+
+from typing import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        r_counter = Counter(ransomNote)
+        m_counter = Counter(magazine)
+        
+        for r_l in r_counter:
+            if r_l not in m_counter or m_counter[r_l] < r_counter[r_l]:
+                return False
+            
+        return True
+    
+solution = Solution()
+print(solution.canConstruct("aa", "aab"))   # true
+
+print(solution.canConstruct("a", "b"))      # false
+
+print(solution.canConstruct("aa", "ab"))      # false
