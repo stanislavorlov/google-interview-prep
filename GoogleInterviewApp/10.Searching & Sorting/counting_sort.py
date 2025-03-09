d22559029ce4188eae89cc28bb1154c5823ffd36
stanislavorlov <stasorlov21@gmail.com>
2025-03-09 14:25:26 +0200

    Linear sorting algorithms

--- GoogleInterviewApp/10.Searching & Sorting/counting_sort.py
+++ GoogleInterviewApp/10.Searching & Sorting/counting_sort.py
@@ -1,9 +1,11 @@
 input_arr = [2,5,3,0,2,3,0,3]
 
+# Θ(k+n)
 def counting_sort(arr):
     max_elem = max(arr)
     count_arr = [0] * (max_elem+1)
 
+    # k
     for num in arr:
         count_arr[num] += 1
     # count_arr contains count of num elements
@@ -14,6 +16,7 @@ def counting_sort(arr):
 
     output = [0] * len(arr)
 
+    # n
     for i in range(len(arr) - 1, -1, -1):
         output[count_arr[arr[i]] - 1] = arr[i]
         count_arr[arr[i]] -= 1
