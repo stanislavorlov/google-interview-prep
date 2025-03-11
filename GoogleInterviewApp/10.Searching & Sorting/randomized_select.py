8467ac2eb26515738badc7da2405c59eeec54ffe
stanislavorlov <stasorlov21@gmail.com>
2025-03-11 14:22:03 +0200

    Randomized search

---
+++ GoogleInterviewApp/10.Searching & Sorting/randomized_select.py
@@ -0,0 +1,40 @@
+import random
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def partition_high(arr, low, high):
+    pivot = arr[high]               # Could arr[low], arr[high], arr[(low+high) //2], arr[random(low, high)]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            swap(arr, i, j)
+
+    swap(arr, i+1, high)
+
+    return i+1
+
+def randomized_partition(arr, low, high):
+    i = random.randrange(low, high)
+    swap(arr, high, i)
+
+    return partition_high(arr, low, high)
+
+# returns the ith smallest element
+def randomized_select(arr, left, right, i):
+    if left == right:
+        return arr[left]
+
+    partition = randomized_partition(arr, left, right)
+    mid = partition - left + 1
+    if i == mid:
+        return arr[partition]
+    elif i < mid:
+        return randomized_select(arr, left, partition-1, i)
+    else:
+        return randomized_select(arr, partition+1, right, i-mid)
+
+array = [-2, 7, 15, -14, 0, 15, 0, 7, -7, -4, -13, 5, 8, -14, 12]
+print(randomized_select(array, 0, len(array)-1, 6))
