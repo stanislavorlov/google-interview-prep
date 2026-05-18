94335fa8ebe38e6ea444e34be1faa6bcf38be92e
stanislavorlov <stasorlov21@gmail.com>
2026-05-18 16:44:54 +0300

    Opeb the lock with breadth first search

--- GoogleInterviewApp/8.Graphs/openLock.py
+++ GoogleInterviewApp/8.Graphs/openLock.py
@@ -35,3 +35,68 @@ class Solution:
                     seen.add(neighbor)
 
         return -1
+
+    def openLock2(self, deadends: List[str], target: str) -> int:
+        next_slot = {
+            '0': '1',
+            '1': '2',
+            '2': '3',
+            '3': '4',
+            '4': '5',
+            '5': '6',
+            '6': '7',
+            '7': '8',
+            '8': '9',
+            '9': '0'
+        }
+        prev_slot = {
+            '0' : '9',
+            '1' : '0',
+            '2' : '1',
+            '3' : '2',
+            '4' : '3',
+            '5' : '4',
+            '6' : '5',
+            '7' : '6',
+            '8' : '7',
+            '9' : '8',
+        }
+
+        visited = set(deadends)
+        pending = deque()
+        turns = 0
+
+        if "0000" in visited:
+            return -1
+
+        pending.append("0000")
+        visited.add("0000")
+
+        while pending:
+            curr_level_count = len(pending)
+            for _ in range(curr_level_count):
+                node = pending.popleft()
+
+                if node == target:
+                    return turns
+
+                for wheel in range(4):
+                    new_combination = list(node)
+                    new_combination[wheel] = next_slot[new_combination[wheel]]
+                    new_combination_str = ''.join(new_combination)
+
+                    if new_combination_str not in visited:
+                        pending.append(new_combination_str)
+                        visited.add(new_combination_str)
+
+                    new_combination = list(node)
+                    new_combination[wheel] = prev_slot[new_combination[wheel]]
+                    new_combination_str = ''.join(new_combination)
+
+                    if new_combination_str not in visited:
+                        pending.append(new_combination_str)
+                        visited.add(new_combination_str)
+
+            turns += 1
+
+        return -1
